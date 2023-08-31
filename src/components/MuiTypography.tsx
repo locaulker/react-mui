5a0fb77043d76ab47212e5cf898747e7956070c0
Lawrence Caulker <locaulker@gmail.com>
2023-08-31 14:49:22 -0400

    end Typography

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+import { Typography } from "@mui/material"
+
+export const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant="h1">h1 Heading</Typography>
+      <Typography variant="h2">h2 Heading</Typography>
+      <Typography variant="h3">h3 Heading</Typography>
+      <Typography variant="h4">h4 Heading</Typography>
+      <Typography variant="h5">h5 Heading</Typography>
+      <Typography variant="h6">h6 Heading</Typography>
+
+      <Typography variant="subtitle1">SubTitle 1</Typography>
+      <Typography variant="subtitle2">SubTitle 2</Typography>
+
+      <Typography variant="body1">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit, a facilis
+        voluptates maxime autem iure? Soluta voluptates recusandae iusto
+        doloremque?
+      </Typography>
+      <br />
+      <Typography variant="body2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+        repellat praesentium similique facere ea hic tenetur quae ipsa quas,
+        magni, voluptates nihil est quaerat quos recusandae ullam corporis, sint
+        inventore!
+      </Typography>
+    </div>
+  )
+}
